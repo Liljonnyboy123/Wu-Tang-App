ed4e4c33940282bca67ab1b826aff07c23862e6a
liljonnyboy123 <joshuaguz76@gmail.com>
2026-04-15 13:05:56 -0700

    Initial Commit

---
+++ Wu-Tang AppTests/Wu_Tang_AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  Wu_Tang_AppTests.swift
+//  Wu-Tang AppTests
+//
+//  Created by GUZMAN, JOSHUA on 4/15/26.
+//
+
+import Testing
+
+struct Wu_Tang_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
